d45a18469d6c611bff1e2f0061a198a6de1dfa48
Elijah Joshua Cayabyab <ebcayabyab@voyagerinnovation.com>
2016-06-17 10:29:56 +0800

    Parse checkout API response

--- lib/PayMaya/API/Checkout.php
+++ lib/PayMaya/API/Checkout.php
@@ -2,16 +2,44 @@
 
 namespace PayMaya\API;
 
-use PayMaya\Model\Checkout\Buyer;
-use PayMaya\Model\Checkout\Item;
-use PayMaya\Model\Checkout\ItemAmount;
+use PayMaya\Core\APIManager;
 
 class Checkout
 {
+	private $checkoutId;
+	private $redirectUrl;
+
 	public $buyer;
 	public $items;
 	public $totalAmount;
 	public $requestReferenceNumber;
 
-	
+	private $apiManager;
+
+	public function __construct()
+	{
+		$this->apiManager = new APIManager();
+	}
+
+	public function execute()
+	{
+		$checkoutInformation = json_decode(json_encode($this), true);
+		$response = $this->apiManager->executeCheckout($checkoutInformation);
+		$responseArr = json_decode($response, true);
+
+		$this->checkoutId = $responseArr["checkoutId"];
+		$this->redirectUrl = $responseArr["redirectUrl"];
+		
+		return $response;
+	}
+
+	public function getCheckoutId() 
+	{
+		return $this->checkoutId;
+	}
+
+	public function getRedirectUrl() 
+	{
+		return $this->redirectUrl;
+	}
 }
