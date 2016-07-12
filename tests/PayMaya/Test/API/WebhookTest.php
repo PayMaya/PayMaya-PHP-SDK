fd85b5301f2db250708227be751675aa2b7ca495
Elijah Joshua Cayabyab <ebcayabyab@voyagerinnovation.com>
2016-07-12 14:17:03 +0800

    Unit tests for APIs

--- tests/PayMaya/Test/API/WebhookTest.php
+++ tests/PayMaya/Test/API/WebhookTest.php
@@ -2,10 +2,64 @@
 
 namespace PayMaya\Test\API;
 
-use PHPUnit\Framework\TestCase;
 use PayMaya\API\Webhook;
 
-class WebhookTest extends TestCase
+class WebhookTest extends \PHPUnit_Framework_TestCase
 {
-	
+	public static function getObject()
+	{
+		$webhook = new Webhook();
+		$webhook->name = Webhook::CHECKOUT_SUCCESS;
+		$webhook->callbackUrl = "http://shop.someserver.com/success";
+		return $webhook;
+	}
+
+	public function testInitialization()
+	{
+		$obj = self::getObject();
+		$this->assertEquals($obj->name, Webhook::CHECKOUT_SUCCESS);
+		$this->assertEquals($obj->callbackUrl, "http://shop.someserver.com/success");
+		return $obj;
+	}
+
+	/**
+	 * @depends testInitialization
+	 */
+	public function testRegister($obj)
+	{
+		$obj->register();
+		$this->assertNotNull($obj->id);
+		return $obj;
+	}
+
+	/**
+	 * @depends testRegister
+	 */
+	public function testRetrieve($obj)
+	{
+		$webhooks = Webhook::retrieve();
+		$this->assertNotEmpty($webhooks);
+	}
+
+	/**
+	 * @depends testRegister
+	 */
+	public function testUpdate($obj)
+	{
+		$obj->callbackUrl = "http://shop.someserver.com/successUpdated";
+		$obj->update();
+		$this->assertEquals($obj->callbackUrl, "http://shop.someserver.com/successUpdated");
+		return $obj;
+	}
+
+	/**
+	 * @depends testUpdate
+	 */
+	public function testDelete($obj)
+	{
+		$obj->delete();
+		$this->assertNull($obj->id);
+		$this->assertNull($obj->name);
+		$this->assertNull($obj->callbackUrl);
+	}
 }
